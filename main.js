58e13af4b4893a9e4ceec235902cd2d7ed32dbeb
Dave Cornelius <d_cornelius94@yahoo.com>
2019-01-17 21:10:08 -0800

    almost finished madlib

---
+++ main.js
@@ -0,0 +1,8 @@
+function word() { 
+    // get input by id
+    console.log('inFunction');
+    var word1Input = document.getElementById('word1');
+    var word2Input = document.getElementById('word2');
+    var word3Input = document.getElementById('word3');
+    var word4Input = document.getElementById('word4'); 
+}
